4c0c65e28bc6b476e6f517e3c36d4bf124c35c4d
Basil Ndonga <basil.ndonga@iprocu.re>
2023-07-22 13:41:44 +0300

    added comments & readme

--- 2-wait-groups/main.go
+++ 2-wait-groups/main.go
@@ -6,11 +6,22 @@ import (
 	"time"
 )
 
+/**
+Wait groups are used to wait for
+go routines to complete execution
+*/
+
 func main(){
+	// create wait group
 	var wg sync.WaitGroup
 
+	// wait for one groutime
 	wg.Add(1)
    
+	/**
+	Wrap call to an anonymous. This syntax creates a function 
+	and immediately invokes it
+	*/
 	go func(){
 		count("dogs")
 		wg.Done()
@@ -21,13 +32,14 @@ func main(){
 		wg.Done()
 	}()
 
-
+    // call wait at the end of the main function
 	wg.Wait()
 }
 
 func count(something string){
 	for i := 1; i <=10; i++ {
 		fmt.Println(i, something)
-		time.Sleep(time.Millisecond * 500)
+
+		time.Sleep(time.Millisecond * 500) // sleep for half a second
 	}
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Go concurrency primitives
+## Contents
+It covers: 
+1. Go routines
+2. Wait groups
+3. Channels
+4. Select Statement
